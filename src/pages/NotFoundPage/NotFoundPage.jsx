3a5a50a780be71b676af652794b727dab7f7d97b
Olha <oyashnaya88@gmail.com>
2025-01-28 19:52:33 +0200

    notf

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,9 @@
 import { Link } from 'react-router-dom';
 import DocumentTitle from '../../components/DocumentTitle/DocumentTitle';
-import css from './NotFoundPage.module.css';
 
 const NoFoundPage = () => {
 	return (
-		<div className={CSS.centering}>
+		<div >
 			<DocumentTitle>Page not found</DocumentTitle>
 			<h1 className={css.title}>404 - Page Not Found</h1>
 			<p>Sorry, the page you are looking for could not be found.</p>
